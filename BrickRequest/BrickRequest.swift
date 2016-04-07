7ce1419150166fc08cf8702b30a64048c23dc61c
muukii <m@muukii.me>
2016-04-08 00:48:38 +0900

    Add License

--- BrickRequest/BrickRequest.swift
+++ BrickRequest/BrickRequest.swift
@@ -1,22 +1,36 @@
+// BrickRequest.swift
 //
-//  File.swift
-//  Pods
+// Copyright (c) 2015 muukii
 //
-//  Created by muukii on 3/24/16.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import Foundation
 import Alamofire
 
 public protocol RequestContextType {
-    
+
 }
 
 extension RequestContextType where Self: ResponseType, Self: RequestType {
-    
+
     public func create(block: Alamofire.Response<SerializedObject, ResponseError> -> Void) -> Alamofire.Request {
-        
+
         let request = self.createRequest(method: self.method, URLString: self.URLString, manager: self.manager)
         request.response(responseSerializer: self.responseSerializer, completionHandler: block)
         return request
@@ -24,7 +38,7 @@ extension RequestContextType where Self: ResponseType, Self: RequestType {
 }
 
 public protocol RequestType {
-    
+
     var method: Alamofire.Method { get }
     var URLString: String { get }
     var manager: Alamofire.Manager { get }
@@ -32,9 +46,9 @@ public protocol RequestType {
 }
 
 public protocol ResponseType {
-    
+
     associatedtype SerializedObject
     associatedtype ResponseError: ErrorType
-    
+
     var responseSerializer: Alamofire.ResponseSerializer<SerializedObject, ResponseError> { get }
 }
